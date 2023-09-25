43f99dc51377cb73a5d758bcbff2084fb69ef249
Bea gyeong seok <gsr025silver.naver.com>
2023-09-25 23:36:48 +0900

    Revert "2023.09.25"
    This reverts commit 17f58e2aada60363c6714ec64abe568870e89fad.

--- app/src/main/java/com/example/healthsns/ProfliePageFragment.kt
+++ app/src/main/java/com/example/healthsns/ProfliePageFragment.kt
@@ -1,17 +1,11 @@
 package com.example.healthsns
 
-import android.app.Dialog
-import android.content.Context
-import android.graphics.Point
-import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.WindowManager
 import android.widget.Button
 import androidx.fragment.app.Fragment
-import com.example.healthsns.DialogUtil.Companion.dialogResize
 
 class ProfliePageFragment:Fragment() {
 
@@ -48,44 +42,10 @@ class ProfliePageFragment:Fragment() {
         }
 
         btn4.setOnClickListener {
-            val dialog4 = Dialog(requireContext(), R.style.CustomDialog)
-            dialog4.setContentView(R.layout.follow_fragment)
-            requireContext().dialogResize(dialog4, 0.8f, 0.6f)
-            dialog4.show()
+            val dialog4 = FollowFragment()
+            dialog4.show(parentFragmentManager, "FollowFragment")
         }
 
         return view
-    }
-
-}
-
-class DialogUtil {
-    companion object {
-        fun Context.dialogResize(dialog: Dialog, width: Float, height: Float){
-            val windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
-
-            if (Build.VERSION.SDK_INT < 30){
-                val display = windowManager.defaultDisplay
-                val size = Point()
-
-                display.getSize(size)
-
-                val window = dialog.window
-
-                val x = (size.x * width).toInt()
-                val y = (size.y * height).toInt()
-
-                window?.setLayout(x, y)
-
-            }else{
-                val rect = windowManager.currentWindowMetrics.bounds
-
-                val window = dialog.window
-                val x = (rect.width() * width).toInt()
-                val y = (rect.height() * height).toInt()
-
-                window?.setLayout(x, y)
-            }
-        }
     }
 }
